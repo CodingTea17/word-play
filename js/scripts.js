9787a14466a76d94c169b09169920978e0b0b801
Dawson Mortenson and Sean Slingerland <dawson@>
2017-08-14 15:05:51 -0700

    make a couple edits to add user functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     // Array od HTML id names
     var itemNames = ["item-1", "item-2", "item-3", "item-4"];
 
-
     // Initializes two empty arrays
     // var groceryList = []; DEPRECIATED!!!!!
     var upperCaseGroceryList = [];
@@ -18,7 +17,6 @@ $(document).ready(function() {
       return ($("#" + suchMunDoges).val()).toUpperCase();
     });
 
-
     // Sorts the newly created grocery array
     // groceryList.sort(); DEPRECIATED!!!!!!
     upperCaseGroceryList.sort();
@@ -27,6 +25,12 @@ $(document).ready(function() {
     //   return greenTiger.toUpperCase(); DEPRECIATED!!!!!!
     // }); DEPRECIATED!!!!!!
 
-    alert(upperCaseGroceryList);
+    $("form#grocery-list").hide();
+
+    upperCaseGroceryList.forEach(function(pinkBigfoot){
+      $("ul#grocery-list-ul").append("<li>" + pinkBigfoot + "</li>");
+    });
+
+    $("div.list-o-groceries").show();
   });
 });
